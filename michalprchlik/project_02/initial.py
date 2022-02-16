d721e19bc07dc4158b377b34f2789b4e5bb8ecda
centOS9 <michal-prchlik@centos.com>
2022-02-16 19:39:06 +0100

    asd

--- michalprchlik/project_02/initial.py
+++ michalprchlik/project_02/initial.py
@@ -2,8 +2,13 @@
 
 import os
 
+username = os.environ.get('GITHUB_ACTOR')
+
+function = getattr(__import__(username, fromlist=[project_02.modules.module]), project_02.modules.module)
 
 def initial():
     return 1
 
 print(os.environ.get('GITHUB_ACTOR'))
+
+function()
