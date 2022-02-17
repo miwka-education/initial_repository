086ecec0b979ba65d3853cd0803aef3f3242f56c
centOS9 <michal-prchlik@centos.com>
2022-02-17 16:54:51 +0100

    asd

--- tests/test_project_02.py
+++ tests/test_project_02.py
@@ -5,7 +5,10 @@ package = os.getenv('GITHUB_ACTOR')
 name = "project_02.modules.module"
 
 
-imported = getattr(__import__(package, fromlist=[name]), name)
+#imported = getattr(__import__(package, fromlist=[name]), name)
+
+name1 = f"{package}.project_02.modules.module"
+imported = __import__(name1)
 
 class TestStringMethods(unittest.TestCase):
 
